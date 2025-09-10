cd9194707df1a22284c36495c62c0401d510b89b
Marcel Scrauwen <marcel.schrauwen@vzvz.nl>
2025-09-10 12:48:45 +0200

    Update displayToc.js

--- docs/js/displayToc.js
+++ docs/js/displayToc.js
@@ -115,13 +115,30 @@ function mapRectangleMouseOver(sender) {
         var tooltipWidth = $(".previewPanel").outerWidth();
         var tooltipHeight = $(".previewPanel").outerHeight();
 
-        // gebruik getBoundingClientRect voor precieze positie
-        var rect = sender.getBoundingClientRect();
-        var scrollTop  = $(window).scrollTop();
-        var scrollLeft = $(window).scrollLeft();
+        // coords van <area>
+        var array = sender.coords.split(',');
+        var x1 = Number(array[0]);
+        var y1 = Number(array[1]);
+        var x2 = Number(array[2]);
+        var y2 = Number(array[3]);
 
-        var left = rect.left + scrollLeft + (rect.width / 2) - (tooltipWidth / 2);
-        var top  = rect.top + scrollTop - tooltipHeight - 2; // 2px marge boven het element
+        // afbeelding waarop <map> zit
+        var mapName = sender.parentNode.name;
+        var imageElem = $('img[usemap="#' + mapName + '"]');
+
+        if (imageElem.length === 0) return;
+
+        var imgOffset = imageElem.offset();
+
+        // horizontaal midden van area
+        var rectCenterX = imgOffset.left + (x1 + x2) / 2;
+
+        // verticaal net boven area
+        var rectTopY = imgOffset.top + y1;
+
+        // tooltip links en boven positie
+        var left = rectCenterX - (tooltipWidth / 2);
+        var top  = rectTopY - tooltipHeight - 2; // 2px marge boven
 
         $(".previewPanel").css({
             "margin-left": left + "px",
