79ffdc6185d43df65b6eece02b83c345f360c123
Marcel Scrauwen <marcel.schrauwen@vzvz.nl>
2025-09-10 16:01:09 +0200

    Update displayToc.js

--- docs/js/displayToc.js
+++ docs/js/displayToc.js
@@ -113,8 +113,8 @@ function mapRectangleMouseOver(sender) {
         // $(".previewPanel").append(taggedValues.html()); // niet meer nodig
 
         $(".previewPanel").css("display", "block");
-        $(".previewPanel").css("margin-top", Number(array[1]) + "px");
-        $(".previewPanel").css("margin-left", (Number(array[2]) - 200) + "px");
+        $(".previewPanel").css("margin-top", Number(array[1]) -100) + "px");
+        $(".previewPanel").css("margin-left", (Number(array[2]) - 50) + "px");
 
     });
 
